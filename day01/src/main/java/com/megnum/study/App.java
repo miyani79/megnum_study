1c2bf510823bc136fd8f156ee6efeb1532860ffe
miyani <miyani79@gmail.com>
2022-04-08 10:55:09 +0900

    마방진 구현중 스타찍기 완료

--- day01/src/main/java/com/megnum/study/App.java
+++ day01/src/main/java/com/megnum/study/App.java
@@ -4,5 +4,68 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("hello world");
+
+        int[][] array = new int[3][3];
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                array[i][j] = 0;
+            }
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                System.out.print( array[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        int colstart = 1;
+        int rowStart = 0;
+        int curNum=1;
+
+        array[rowStart][colstart] = curNum++;
+
+        //대각선이동 후 값채우기
+        rowStart = rowStart - 1;
+        colstart = colstart + 1;
+
+        if (rowStart < 0) {
+            rowStart = array.length -1;
+        }
+
+        array[rowStart][colstart] = curNum++;
+
+        //대각선이동 후 값채우기
+        rowStart = rowStart - 1;
+        colstart = colstart + 1;
+
+        if (colstart >= array.length) {
+            colstart = 0;
+        }
+
+        array[rowStart][colstart] = curNum++;
+
+        //대각선이동 후 값채우기
+        rowStart = rowStart - 1;
+        colstart = colstart + 1;
+
+        if (array[rowStart][colstart] != 0) {
+            rowStart = rowStart + 1;
+            colstart = colstart - 1;
+            rowStart = rowStart + 1;
+        }
+
+        array[rowStart][colstart] = curNum++;
+
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                System.out.print( array[i][j]);
+            }
+            System.out.println();
+        }
+
     }
 }
